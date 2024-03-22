7b5f560f6aa44a1d0a52225d1afd926a84b4e1b2
Romisaa <romisaa.hamdy@yahoo.com>
2024-03-22 07:46:40 -0700

    all

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-# installs the package puppet-lint
-package { 'puppet-lint':
-  ensure   => '2.1.1',
-  provider => 'gem',
+# Define the package resource for Flask
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
