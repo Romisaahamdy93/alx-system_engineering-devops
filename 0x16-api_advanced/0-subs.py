5cbec9101afc7b2b7a7aedb1b47098b041254226
ٌRomisaa <romisaa.hamdy@yahoo.com>
2024-05-10 10:12:34 -0700

    edit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,23 +1,20 @@
 #!/usr/bin/python3
-"""Function to query subscribers on a given Reddit subreddit."""
+"""
+Queries the Reddit API and returns the number of total subscribers for a given
+subreddit.
+"""
 import requests
 
+
 def number_of_subscribers(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "Custom User Agent"}  # Set a custom User-Agent header
-    response = requests.get(url, headers=headers, allow_redirects=False)
-
-    # Check if the response is successful and contains valid JSON data
-    if response.status_code == 200:
-        try:
-            data = response.json()
-            if 'data' in data and 'subscribers' in data['data']:
-                return data['data']['subscribers']
-        except ValueError:
-            pass  # Invalid JSON, fall through to return 0
-    
-    return 0
-
-if __name__ == "__main__":
-    subreddit = input("Enter subreddit: ")
-    print(number_of_subscribers(subreddit))
+    """
+    Queries the Reddit API and returns the number of total subscribers for a
+    given subreddit.
+    """
+    url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'Python/1.0(Holberton School 0x16 task 0)'}
+    response = requests.get(url, headers=headers)
+    if (not response.ok):
+        return 0
+    subscriber_count = response.json().get('data').get('subscribers')
+    return subscriber_count
