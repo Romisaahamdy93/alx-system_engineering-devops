630b9ed09674ca24ff6ae1d43001586b7fd5f84b
ٌRomisaa <romisaa.hamdy@yahoo.com>
2024-05-10 08:57:21 -0700

    edit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,23 +1,19 @@
+#!/usr/bin/python3
+"""
+Script that queries subscribers on a given Reddit subreddit.
+"""
+
 import requests
 
-def number_of_subscribers(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "Custom User Agent"}  # Set a custom User-Agent header
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        response.raise_for_status()  # Raise an exception for HTTP errors
-        data = response.json()
-        if 'data' in data and 'subscribers' in data['data']:
-            return data['data']['subscribers']
-    except requests.exceptions.HTTPError as e:
-        if e.response.status_code == 404:
-            return 0
-        else:
-            raise  # Re-raise the exception if it's not a 404 error
-    except Exception as e:
-        print("An error occurred:", e)
-    return 0
 
-if __name__ == "__main__":
-    subreddit = input("Enter subreddit: ")
-    print(number_of_subscribers(subreddit))
+def number_of_subscribers(subreddit):
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
+        return 0
